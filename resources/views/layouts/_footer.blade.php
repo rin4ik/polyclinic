96be7db52704b0df3b86a617d7c82d5dd8f425ee
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-04-22 15:32:59 +0300

    new footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,46 @@
-    <!-- Footer -->
-    <footer class="py-3 bg-dark "  >
-            <div class="container">
-                <p class="m-0 text-center text-white"> &copy; OILAVIY POLIKLINIKA 2018</p>
+<footer id="myFooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h2 class="logo"><a href="#"><img src="{{asset('images/logo11.png')}}" alt=""  ></a></h2>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Polilinika</h5>
+                    <ul>
+                        <li><a href="/">Bosh sahifa</a></li>
+                        <li><a href="/yangiliklar">Yangiliklar</a></li>
+           
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Biz xaqimizda</h5>
+                    <ul>
+                        <li><a href="/xodimlar">Xodimlar</a></li>
+                        <li><a href="/foto">Foto</a></li>
+                        <li><a href="/aloqa">Aloqa</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Ma'lumot</h5>
+                    <ul>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                     
+                    <div class="social-networks">
+                            <small style="grey;  ">Sayt muallifi bilan aloqa: </small> <p style="color:rgb(255, 76, 76);  ">mirahmad@develax.io</p>
+                        <a href="https://vk.com/rin4ikk" class="vk"><i class="fa fa-vk"></i></a>
+                        <a href="https://www.facebook.com/profile.php?id=100005089215072" class="facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="https://twitter.com/_rin4ik_" class="twitter"><i class="fa fa-twitter"></i></a>
+                    </div>
+                    
+                </div>
             </div>
-            <!-- /.container -->
-        </footer>
+        </div>
+        <div class="footer-copyright">
+            <p>© 2018 Oilaviy Poliklinika </p>
+        </div>
+    </footer>
