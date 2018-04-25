2433439e31be455700a95aaed37de28770a3dab2
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-04-25 12:24:33 +0300

    contact map

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -3,31 +3,26 @@
  
 <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-6"> 
+            <div class="col-md-9"> 
     
                    <!-- Card -->
-<div class="card">
-        
+<div class="card"> 
           <!-- Card image -->
-          <div class="view overlay">
-            <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" alt="Card image cap">
-            <a href="#!">
-              <div class="mask rgba-white-slight"></div>
-            </a>
-          </div>
         
           <!-- Card content -->
           <div class="card-body">
-        
-            <!-- Title --> 2449913
-            <h4 class="card-title">Card title</h4>
+            <h4 class="card-title">Fargona Shahri 5-sonli Oilaviy Poliklinika</h4>
             <!-- Text -->
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text">Yuksalish kochasi 63-uy</p>
+            <strong>Telefon raqamlarimiz:</strong>  2249539, 2249538 <br><br>
+            <strong>Ish kunlari:</strong> Shanbadan tashqari xar kuni
             <!-- Button -->
-            <a href="#" class="btn btn-primary">Button</a>
-         
+        
+            
+ 
         <!-- Card -->
                 </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d887.379560930191!2d71.79721079234798!3d40.37990923596958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bb84841c469a1b%3A0x441cb41746b06343!2z0YPQuy4g0K7QutGB0LDQu9C40YgsINCk0LXRgNCz0LDQvdCwLCDQo9C30LHQtdC60LjRgdGC0LDQvQ!5e0!3m2!1sru!2s!4v1524610068784" width="825" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
         </div>
     </div>
