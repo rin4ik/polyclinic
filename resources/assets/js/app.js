79385d0b799dae9e53bc0b2e927c1bc03538356c
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-06-30 10:06:47 +0300

    fix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,3 @@
-import VModal from 'vue-js-modal';
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
